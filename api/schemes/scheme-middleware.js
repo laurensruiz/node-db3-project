e99390e4a74a503cdc372b6e4a117578cad38b12
laurensruiz <laurenruiz97@yahoo.com>
2022-05-18 10:33:54 -0400

    didcheckSchemeid

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,4 @@
+const db = require('../../data/db-config')
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,8 +7,21 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
+const checkSchemeId = async (req, res, next) => {
+  try{
+    const existing = await db('schemes')
+    .where('scheme_id', req.params.scheme_id,).first()
 
+    if(!existing){
+      next({
+        status: 404, 
+        message: `scheme with scheme_id ${req.params.scheme_id} not found `})
+    } else {
+      next()
+    }
+  } catch (err){
+    next(err)
+  }
 }
 
 /*
@@ -19,7 +33,20 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
+  try{
+    const existing = await db('schemes')
+    .where('scheme_name', req.params.scheme_name,).first()
 
+    if(!existing){
+      next({
+        status: 400, 
+        message: 'invalid scheme_name'})
+    } else {
+      next()
+    }
+  } catch (err){
+    next(err)
+  }
 }
 
 /*
